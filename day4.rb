0a2380f3e1a92bf39062f87ba3f6760f8d819340
Konrad Winkler <konwinkler@gmail.com>
2023-12-06 20:15:04 -0500

    day 4 part 2

--- day4.rb
+++ day4.rb
@@ -50,3 +50,53 @@ end
 
 test_equals points('input4.test.txt'), 13
 puts points('input4.txt')
+
+def calculate_counts(wins)
+    # initialize all cards with count 1
+    counts = wins.map {|win| 1}
+    # add up the copies
+      # card 1, count 1 -> adds 1 to cards 2, 3, 4, 5
+      # card 2, count 2 -> adds 2 to cards 3, 4
+    wins.each_with_index {|hits, index|
+        current_count = counts[index]
+        (index+1..index+hits).to_a.each {|add_index|
+            if (add_index < counts.length)
+                counts[add_index] = counts[add_index] + current_count
+            end
+        }
+    }
+    counts
+end
+test_equals calculate_counts([1]), [1]
+test_equals calculate_counts([1, 1]), [1, 2]
+test_equals calculate_counts([2, 1, 1]), [1, 2, 4]
+
+def total_scratchcards(file_name)
+    # calculate for each cards how many wins they get
+      # card 1 wins 4 times
+      # card 2 wins 2 times
+      lines = read_file(file_name)
+      wins = []
+      lines.each_with_index {|line, line_index|
+          numbers = line.split(': ')[1]
+          left, right = numbers.split('|')
+          winning = left.split(' ').map {|number| number.to_i}
+          owned = right.split(' ').map {|number| number.to_i}
+          hits = 0
+          owned.each {|number|
+              if winning.include?(number)
+                  hits = hits + 1
+              end
+          }
+          wins.push(hits)
+      }
+
+    counts = calculate_counts(wins)
+    
+
+    # sum all counts at the end
+    counts.sum
+end
+
+test_equals total_scratchcards('input4.test.txt'), 30
+puts total_scratchcards('input4.txt')
